87e36ee83d3e076be19fcb2b611ea30a148e8668
AlejandroFloresV <alejandroflores.af@gmail.com>
2014-02-28 00:26:02 -0430

    change check

--- check.sh
+++ check.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 for i in {0..19}
 do
-	for j in {1..12}
-	do
-		q=`printf "%02d" $j`
-		diff log/DEX-10M-Q$q-$i.log log/Neo4j-10M-Q$q-$i.log
-	done
+	diff log/DEX-10M-$1-$i.log log/Neo4j-10M-$1-$i.log
 done
