e2150aecbc2da54b3c4a2cfbd41027d913b22780
Alejandro Flores V <alejandroflores.af@gmail.com>
2015-06-27 11:06:46 -0430

    ups..

--- ve/usb/ldc/graphium/tools/DegreeSet.java
+++ ve/usb/ldc/graphium/tools/DegreeSet.java
@@ -16,7 +16,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-package ve.usb.ldc.graphium.chrysalis;
+package ve.usb.ldc.graphium.tools;
 
 import java.util.*;
 import java.lang.*;
